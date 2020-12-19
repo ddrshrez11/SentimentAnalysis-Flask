2da792e0980241d31814e91fc24045e5048b0860
Yunip Shrestha <yunipshrestha@gmail.com>
2020-12-19 17:15:16 +0545

    myenv removed

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__=='__main__':
     app.run(debug=True)
